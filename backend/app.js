ed07bb9942b1b0573579372ef0cb4236ef1e32b1
Tran Minh Khanh <kidopro03@gmail.com>
2024-04-28 15:24:24 +0700

    Try to get sth (5)

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
+const { log } = require("console");
 const express = require("express")
-
+const fs = require("fs")
 const app = express()
 
 // app.get('/',(req,res) => {
@@ -10,8 +11,24 @@ const app = express()
 //     res.status(201).json({message: 'Data created!'})
 // })
 
-// //GET request
-// app.get('/api/v1/')
+//GET request
+const nfts = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/nft-simple.json`)
+);
+
+console.log('====================================');
+console.log(nfts);
+console.log('====================================');
+
+app.get('/api/v1/nfts', (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: nfts.length,
+        data: {
+            nfts,
+        }
+    })
+})
 
 const port = 3000
 app.listen(port, () => {
