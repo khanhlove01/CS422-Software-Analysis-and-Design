286f31e20b941e8e417c164b3eea500c821eabd0
hongnhut2404 <nhutth2404@gmail.com>
2024-04-30 01:38:53 +0700

    setup the structure

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//Internal import
+import Style from './NavBar.module.css'
+
+const NavBar = () => {
+  return (
+    
+    <div>NavBar</div>
+  )
+}
+
+
+export default NavBar
