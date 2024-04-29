98755667739923a6fad865693e52c213c6b0928d
Tran Minh Khanh <kidopro03@gmail.com>
2024-04-29 22:15:35 +0700

    Update NFT 20 model

--- backend/models/nftModel.js
+++ backend/models/nftModel.js
@@ -4,16 +4,55 @@ const nftSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A nft must have a name'],
+        trim: true,
         unique: true
     },
-    rating: {
+    duration:{
+        type: String,
+        required: [true, 'A nft must have a duration'] 
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A nft must have a group size']
+    },
+    difficulty:{
+        type: String,
+        required: [true, 'A nft must have a difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price:{
         type: Number,
         required: [true, 'A nft must have a price']
     },
+    priceDiscount:{
+        type: Number
+    },
+    summary:{
+        type:String,
+        trim: true,
+        required: [true, 'A nft must have a summary']
+    },
+    description:{
+        type:String,
+        trim: true
+    },
+    imageCover:{
+        type: String,
+        required: [true, 'A nft must have a cover image']
+    },
+    images: [String],
+    createdAt:{
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date],
 })
 
 const NFT = mongoose.model('NFT', nftSchema);
