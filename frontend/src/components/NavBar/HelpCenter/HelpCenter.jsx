6229322fc3aee6d9f887692da1c0bb0d39b08bc0
hongnhut2404 <nhutth2404@gmail.com>
2024-04-30 16:45:24 +0700

    create helpCenter component

--- frontend/src/components/NavBar/HelpCenter/HelpCenter.jsx
+++ frontend/src/components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Style from "./HelpCenter.module.css";
 
 const HelpCenter = () => {
+  const helpCenter = [
+    {
+      name: "About",
+      link: "about",
+    },
+    {
+      name: "Contact Us",
+      link: "contact-us",
+    },
+    {
+      name: "Sign In",
+      link: "sign-in",
+    },
+    {
+      name: "Subscription",
+      link: "subscription",
+    },
+  ]
+
+
   return (
-    <div>HelpCenter</div>
+    <div>
+      {helpCenter.map((item, index) => (
+        <div key={index + 1} className={Style.helpCenter}>
+          {/* <link rel="stylesheet" href={`${item.link}`}>{item.name}</link> */}
+          <div>{item.name}</div>
+        </div>
+      ))}
+    </div>
   )
 }
 
