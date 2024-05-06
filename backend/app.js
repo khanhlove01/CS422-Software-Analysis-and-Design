f820ec72070cf728e3de2f0ac3d7cc8695107a21
Tran Minh Khanh <kidopro03@gmail.com>
2024-05-06 16:17:30 +0700

    Error section

--- backend/app.js
+++ backend/app.js
@@ -26,4 +26,13 @@ app.use((req,res,next) => {
 app.use('/api/v1/nfts', nftsRouter);
 app.use('/api/v1/users', usersRouter);
 
+//ERROR SECTION
+
+app.all('*', (req,res,next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+})
+
 module.exports = app;
