8c9ba0ad8ef188db3bd34540b1dead9ff6c8243c
hongnhut2404 <nhutth2404@gmail.com>
2024-05-06 16:53:12 +0700

    create BigNFTSlider component

---
+++ frontend/src/components/BigNFTSlider/BigNFTSlider.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AiFillFire, AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { MdVerified, MdTimer } from 'react-icons/md';
+import { TbArrowBigLeftLine, TbArrowBigRightLine } from 'react-icons/tb';
+
+//Internal Import
+import Style from "./BigNFTSlider.module.css"
+import { Button } from '../componentsindex';
+
+//IMAGE Import
+import test_img from "../../img/tmp.png";
+
+
+const BigNFTSlider = () => {
+  return (
+    <div>BigNFTSlider</div>
+  )
+}
+
+export default BigNFTSlider
