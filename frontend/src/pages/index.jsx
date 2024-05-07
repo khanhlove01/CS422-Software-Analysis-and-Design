7e06f593debf2ce0bf24d7bb60eb07314b4e4544
hongnhut2404 <nhutth2404@gmail.com>
2024-05-08 01:00:00 +0700

    basic layout for filter component

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,20 +2,35 @@ import React from "react";
 
 //Internal Import
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category} from "../components/componentsindex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+} from "../components/componentsindex";
 
-
-const Home = () =>{
-    return(
-        <div className={Style.homePage}>
-            <HeroSection/>
-            <Service/>
-            <BigNFTSlider/>
-            <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories"/>
-            <Category/>
-            <Subscribe/>
-        </div>
-    )
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSlider />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life"
+      />
+      <Filter/>
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories"
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  );
 };
 
 export default Home;
