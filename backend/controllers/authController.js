b09d1872cd147776591c0d49ea067be306d2bbb1
Tran Minh Khanh <kidopro03@gmail.com>
2024-05-09 00:37:38 +0700

    Save jwt to cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,6 +15,16 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
 
+    res.cookie('jwt', token, {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        // secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    });
+
+    user.password = undefined;
+
+    //if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
     res.status(statusCode).json({
         status: 'success',
         token,
