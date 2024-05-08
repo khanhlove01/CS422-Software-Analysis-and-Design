d7b670d5c75f20823e63e7b23142825d88e401d7
hongnhut2404 <nhutth2404@gmail.com>
2024-05-09 01:51:49 +0700

    add style for banner

--- frontend/src/collectionPage/Banner/Banner.jsx
+++ frontend/src/collectionPage/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = ({ bannerImage }) => {
           objectFit="cover"
           alt="background"
           width={1600}
-          height={100}
+          height={400}
         />
       </div>
 
