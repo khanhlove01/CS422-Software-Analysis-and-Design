b3e0570ae07fd3bd3a97f267f8fc46da321c39d4
hongnhut2404 <nhutth2404@gmail.com>
2024-05-09 21:27:08 +0700

    add style NFT Imgae

--- frontend/src/nftDetailsPage/NFTDetailsPage.jsx
+++ frontend/src/nftDetailsPage/NFTDetailsPage.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
 //Internal Import
-import { NFTDescription, NFTDetailsImg, NFTTabs } from './NFTDetailsIndex'
-import Style from "./NFTDetailsPage.module.css"
+import { NFTDescription, NFTDetailsImg, NFTTabs } from "./NFTDetailsIndex";
+import Style from "./NFTDetailsPage.module.css";
 
 const NFTDetailsPage = () => {
   return (
     <div className={Style.NFTDetailsPage}>
-    <div className={Style.NFTDetailsPage_box}>
-      <NFTDetailsImg  />
-      <NFTDescription />
+      <div className={Style.NFTDetailsPage_box}>
+        <NFTDetailsImg />
+        <NFTDescription />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default NFTDetailsPage
+export default NFTDetailsPage;
--- frontend/src/nftDetailsPage/NFTTabs/NFTTabs.jsx
+++ frontend/src/nftDetailsPage/NFTTabs/NFTTabs.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from "react";
 
-//Internal Import
-import Style from "./NFTTabs.module.css"
+//INTERNAL IMPORT
+import Style from "./NFTTabs.module.css";
 
-const NFTTabs = () => {
+const NFTTabs = ({ dataTab, icon }) => {
   return (
-    <div>NFTTabs</div>
-  )
-}
+    <div className={Style.NFTTabs}>
+      {dataTab.map((el, i) => (
+        <div className={Style.NFTTabs_box} key={i + 1}>
+          <img
+            src={el}
+            alt="profile image"
+            width={40}
+            height={40}
+            className={Style.NFTTabs_box_img}
+          />
+          <div className={Style.NFTTabs_box_info}>
+            <span>
+              Offer by $770 by <span>Shoaib Bhai</span>
+              {icon}
+            </span>
 
-export default NFTTabs
+            <small>Jun 14 - 4:12 PM</small>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NFTTabs;
