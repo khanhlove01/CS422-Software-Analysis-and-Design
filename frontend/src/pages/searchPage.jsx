959c911b6583f574a3c025a67dea0816bbf5673b
hongnhut2404 <nhutth2404@gmail.com>
2024-05-09 20:03:08 +0700

    add style SearchBar component

--- frontend/src/pages/searchPage.jsx
+++ frontend/src/pages/searchPage.jsx
@@ -10,11 +10,24 @@ import { NFTCardTwo, Banner } from "../collectionPage/collectionindex";
 import images from "../img/index"
 
 const SearchPage = () => {
+  const collectionArray = [
+    images.test_img,
+    images.test_img_2,
+    images.test_img_3,
+    images.test_img,
+    images.test_img_2,
+    images.test_img_3,
+    images.test_img,
+    images.test_img_2,
+    images.test_img_3,
+    
+  ]
   return (
     <div className={Style.searchBar}>
-      <Banner/>
+      <Banner bannerImage={images.test_img_3}/>
       <Filter/>
       <SearchBar/>
+      <NFTCardTwo NFTData={collectionArray}/>
       <Slider/>
       <Brand/>
     </div>
--- frontend/src/searchPage/SearchBar/SearchBar.jsx
+++ frontend/src/searchPage/SearchBar/SearchBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { BsSearch, BsArrowRight } from "react-icons/bs";
+
+//INTERNAL IMPORT
+import Style from "./SearchBar.module.css";
 
 const SearchBar = () => {
+  
   return (
-    <div>SearchBar</div>
+    <div className={Style.SearchBar}>
+      <div className={Style.SearchBar_box}>
+        <BsSearch className={Style.SearchBar_box_icon} />
+        <input
+          type="text"
+          placeholder="Type yout keyword..."
+          onChange={(e) => {}}
+          value={""}
+        />
+        <BsArrowRight className={Style.SearchBar_box_icon} />
+      </div>
+    </div>
   )
 }
 
