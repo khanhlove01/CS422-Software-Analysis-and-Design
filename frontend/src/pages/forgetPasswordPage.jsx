c4647cee979a097cac84c62f5574e1e2a2b435d4
Tran Minh Khanh <kidopro03@gmail.com>
2024-05-13 16:40:34 +0700

    Sent token to email successfully

--- frontend/src/pages/forgetPasswordPage.jsx
+++ frontend/src/pages/forgetPasswordPage.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import { useState } from 'react';
 //INTERNAL IMPORT
 import Style from "../styles/login.module.css";
 import images from "../img/index";
 import { Button } from "../components/componentsindex";
+import axios from 'axios';
 
 const ForgetPasswordPage = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+  });
+
+const handleChange = (e) => {
+  setInputs((preV) => ({ ...preV, [e.target.name]: e.target.value }));
+};
+
+const handleForgetPassword = async (e) => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/v1/users/forgotPassword",{
+        email: inputs.email
+      })
+      e.preventDefault();
+      console.log("Sent successful!");
+    } catch (error) {
+      console.log(error);
+    }
+}
+
   return (
     <div className={Style.login}>
       <div className={Style.login_box}>
@@ -24,13 +45,13 @@ const ForgetPasswordPage = () => {
                   type="email" 
                   placeholder="example@emample.com"
                   name = "email"
-                //   onChange={handleChange}
-                //   value={inputs.email}
+                  onChange={handleChange}
+                  value={inputs.email}
                 />
               </div>
             </div>
 
-            <Button btnName="Submit" classStyle={Style.button} handleClick={()=>{}}/>
+            <Button btnName="Submit" classStyle={Style.button} handleClick={handleForgetPassword}/>
             {/* <button onClick={handleLogin}>Submit</button> */}
           </div>
         </div>
