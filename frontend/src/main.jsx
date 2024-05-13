b9c792ceee7d466cc5578bace00100dea3377c5f
Tran Minh Khanh <kidopro03@gmail.com>
2024-05-13 14:20:40 +0700

    Update login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { AuthContextProvider } from "./context/authContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,10 +1,44 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { useState } from "react";
 //INTERNAL IMPORT
 import Style from "../styles/login.module.css";
 import LoginAndSignUp from "../loginAndSignUp/LoginAndSignUp";
-
+import { AuthContext } from "../context/authContext";
+import { ToastContainer, toast } from "react-toastify";
 const loginPage = () => {
+
+  const navigate = useNavigate();
+  const { currentUser, login, logout } = useContext(AuthContext);
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((preV) => ({ ...preV, [e.target.name]: e.target.value }));
+  };
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await login({
+  //       userId: inputs.username,
+  //       password: inputs.password,
+  //     });
+
+  //     if (res) {
+  //       navigate("/");
+  //     }
+  //   } catch (err) {
+  //     console.log("====================================");
+  //     console.log(err);
+  //     toast.error("Please input again!!!")
+  //     console.log("====================================");
+  //     setInputs("")
+  //   }
+  // };
   return (
     <div className={Style.login}>
       <div className={Style.login_box}>
