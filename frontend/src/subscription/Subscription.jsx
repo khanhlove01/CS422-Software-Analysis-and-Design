17859f6679868628f6a61ce37f82697394ce5db6
hongnhut2404 <nhutth2404@gmail.com>
2024-05-13 15:37:40 +0700

    update Subscription

--- frontend/src/subscription/Subscription.jsx
+++ frontend/src/subscription/Subscription.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 import { TiTick } from "react-icons/ti";
 
 //Internal Import
-import Style from "./Subscription.module.css"
-import { Button } from '../components/componentsindex';
+import Style from "./Subscription.module.css";
+import { Button } from "../components/componentsindex";
 
 const Subscription = ({ el, i }) => {
-    return (
-      <div className={Style.SubscriptionBox}>
-        <div className={Style.SubscriptionBox_box}>
+  return (
+    <div className={Style.SubscriptionBox}>
+      <div className={Style.SubscriptionBox_box}>
+        <div className={Style.SubscriptionBox_box_container}>
           <span className={Style.SubscriptionBox_box_span}>{el.plan}</span>
-          <small className={Style.SubscriptionBox_box_small}>
-            {el.popular || ""}
-          </small>
+          {el.popular === "" ? null : (
+            <small className={Style.SubscriptionBox_box_small}>
+              {el.popular}
+            </small>
+          )}
+
           <p className={Style.SubscriptionBox_box_price}>{el.price}</p>
-  
+
           <div className={Style.SubscriptionBox_box_info}>
             {el.service.map((el, i) => (
               <p className={Style.SubscriptionBox_box_info_para} key={i + 1}>
@@ -25,14 +29,16 @@ const Subscription = ({ el, i }) => {
               </p>
             ))}
           </div>
-          <Button
-            btnName="Submit"
-            handleClick={() => {}}
-            classStyle={Style.button}
-          />
         </div>
+
+        <Button
+          btnName="Submit"
+          handleClick={() => {}}
+          classStyle={Style.button}
+        />
       </div>
-    );
-  };
-  
-  export default Subscription;
+    </div>
+  );
+};
+
+export default Subscription;
