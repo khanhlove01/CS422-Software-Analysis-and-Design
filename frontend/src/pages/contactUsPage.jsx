b326c646dab6af7c0ff4fd4a4a8de999515267cd
hongnhut2404 <nhutth2404@gmail.com>
2024-05-17 11:24:18 +0700

    fix style for ContactUS page

--- frontend/src/pages/contactUsPage.jsx
+++ frontend/src/pages/contactUsPage.jsx
@@ -7,92 +7,108 @@ import {
   TiSocialInstagram,
 } from "react-icons/ti";
 import { HiOutlineMail } from "react-icons/hi";
+import { MdOutlineSpeakerNotes, MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { FaEarthAmericas } from "react-icons/fa6";
 
 //Internal Import
-import Style from "../styles/contactUsPage.module.css"
+import Style from "../styles/contactUsPage.module.css";
 import { Button } from "../components/componentsindex";
 
 const ContactUsPage = () => {
-    return (
-        <div className={Style.contactus}>
-          <div className={Style.contactus_box}>
-            <h1>Contact</h1>
-            <div className={Style.contactus_box_box}>
-              <div className={Style.contactus_box_box_left}>
-                <div className={Style.contactus_box_box_left_item}>
-                  <h3>🗺 ADDRESS</h3>
-                  <p>
-                    Photo booth tattooed prism, portland taiyaki hoodie neutra
-                    typewriter
-                  </p>
-                </div>
-                <div className={Style.contactus_box_box_left_item}>
-                  <h3>💌 EMAIL</h3>
-                  <p>nc.example@example.com</p>
-                </div>
-                <div className={Style.contactus_box_box_left_item}>
-                  <h3>☎ PHONE</h3>
-                  <p>000-123-456-7890</p>
-                </div>
-                <div className={Style.contactus_box_box_left_item}>
-                  <h3>🌏 SOCIALS</h3>
-                  <a href="#">
-                    <TiSocialFacebook />
-                  </a>
-                  <a href="#">
-                    <TiSocialLinkedin />
-                  </a>
-                  <a href="#">
-                    <TiSocialInstagram />
-                  </a>
-                  <a href="#">
-                    <TiSocialYoutube />
-                  </a>
-                  <a href="#">
-                    <TiSocialTwitter />
-                  </a>
-                </div>
-              </div>
-              <div className={Style.contactus_box_box_right}>
-                <form>
-                  <div className={Style.contactus_box_input}>
-                    <label htmlFor="name">Full Name</label>
-                    <input
-                      type="text"
-                      placeholder="shoaib bhai"
-                      className={Style.contactus_box_input_userName}
-                    />
-                  </div>
-                  <div className={Style.contactus_box_input}>
-                    <label htmlFor="email">Email</label>
-                    <div className={Style.contactus_box_input_box}>
-                      <div className={Style.contactus_box_input_box_icon}>
-                        <HiOutlineMail />
-                      </div>
-                      <input type="text" placeholder="Email*" />
-                    </div>
-                  </div>
-                  <div className={Style.contactus_box_input}>
-                    <label htmlFor="description">Message</label>
-                    <textarea
-                      name=""
-                      id=""
-                      cols="30"
-                      rows="6"
-                      placeholder="something about yourself in few words"
-                    ></textarea>
-                  </div>
-                  <Button
-                    btnName="Send Message"
-                    handleClick={() => {}}
-                    classStyle={Style.button}
-                  />
-                </form>
+  return (
+    <div className={Style.contactus}>
+      <div className={Style.contactus_box}>
+        <h1>Contact</h1>
+        <div className={Style.contactus_box_box}>
+          <div className={Style.contactus_box_box_left}>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>
+                <MdOutlineSpeakerNotes className={Style.icon} /> ADDRESS
+              </h3>
+              <p>
+                Photo booth tattooed prism, portland taiyaki hoodie neutra
+                typewriter
+              </p>
+            </div>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>
+                <MdEmail className={Style.icon} /> EMAIL
+              </h3>
+              <p>nc.example@example.com</p>
+            </div>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>
+                <FaPhone className={Style.icon} /> PHONE
+              </h3>
+              <p>000-123-456-7890</p>
+            </div>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>
+                <FaEarthAmericas className={Style.icon} />
+                SOCIALS
+              </h3>
+              <div className={Style.contactus_box_box_left_item_social}>
+                <a href="#">
+                  <TiSocialFacebook />
+                </a>
+                <a href="#">
+                  <TiSocialLinkedin />
+                </a>
+                <a href="#">
+                  <TiSocialInstagram />
+                </a>
+                <a href="#">
+                  <TiSocialYoutube />
+                </a>
+                <a href="#">
+                  <TiSocialTwitter />
+                </a>
               </div>
             </div>
           </div>
+          <div className={Style.contactus_box_box_right}>
+            <form>
+              <div className={Style.contactus_box_input}>
+                <label htmlFor="name">Full Name</label>
+                <input
+                  type="text"
+                  placeholder="shoaib bhai"
+                  className={Style.contactus_box_input_userName}
+                />
+              </div>
+              <div className={Style.contactus_box_input}>
+                <label htmlFor="email">Email</label>
+                <div className={Style.contactus_box_input_box}>
+                  <div className={Style.contactus_box_input_box_icon}>
+                    <HiOutlineMail />
+                  </div>
+                  <input type="text" placeholder="Email*" />
+                </div>
+              </div>
+              <div className={Style.contactus_box_input}>
+                <label htmlFor="description">Message</label>
+                <textarea
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="6"
+                  placeholder="something about yourself in few words"
+                ></textarea>
+              </div>
+              <div className={Style.button_container}>
+                <Button
+                  btnName="Send Message"
+                  handleClick={() => {}}
+                  className={Style.button}
+                />
+              </div>
+            </form>
+          </div>
         </div>
-      );
-}
+      </div>
+    </div>
+  );
+};
 
-export default ContactUsPage
+export default ContactUsPage;
