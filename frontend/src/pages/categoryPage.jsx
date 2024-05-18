739d5ca90c6886186ca989784a94113b1ef98890
hongnhut2404 <nhutth2404@gmail.com>
2024-05-19 01:41:30 +0700

    call API for Category Page

--- frontend/src/pages/categoryPage.jsx
+++ frontend/src/pages/categoryPage.jsx
@@ -1,24 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
 
 // Internal Import
 import Style from "../styles/categoryPage.module.css";
-import { Banner, CollectionProfile } from "../collectionPage/collectionindex";
-import { NFTCardTwo } from "../collectionPage/collectionindex";
-import { GiConsoleController } from "react-icons/gi";
+import { Banner, CollectionProfile, NFTCardTwo } from "../collectionPage/collectionindex";
 
 const CategoryPage = () => {
   const { id } = useParams();
+  const [dataArray, setDataArray] = useState([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/v1/nfts");
+        setDataArray(res.data.data.nfts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const collections = {
     forest: {
       bannerImage:
         "https://pbs.twimg.com/media/FyqXPJkaAAAVG56?format=jpg&name=4096x4096",
-        profileImage: "https://pbs.twimg.com/media/FyqXPJkaAAAVG56?format=jpg&name=4096x4096",
+      profileImage:
+        "https://pbs.twimg.com/media/FyqXPJkaAAAVG56?format=jpg&name=4096x4096",
       collectionArray: [
         "https://pbs.twimg.com/media/GJ_bZMDasAAQ26H?format=jpg&name=4096x4096",
         "https://pbs.twimg.com/media/GHa18RLagAATUZE?format=jpg&name=4096x4096",
@@ -28,7 +42,8 @@ const CategoryPage = () => {
     sky: {
       bannerImage:
         "https://pbs.twimg.com/media/EFhnhYcVUAA42I0?format=jpg&name=4096x4096",
-        profileImage: "https://pbs.twimg.com/media/EFhnhYcVUAA42I0?format=jpg&name=4096x4096",
+      profileImage:
+        "https://pbs.twimg.com/media/EFhnhYcVUAA42I0?format=jpg&name=4096x4096",
       collectionArray: [
         "https://pbs.twimg.com/media/GDUMIUMb0AA7rXO?format=jpg&name=4096x4096",
         "https://pbs.twimg.com/media/GDUMLDgbYAA3nJu?format=jpg&name=4096x4096",
@@ -38,7 +53,8 @@ const CategoryPage = () => {
     sea: {
       bannerImage:
         "https://pbs.twimg.com/media/GCfHyvCaUAAODgW?format=jpg&name=4096x4096",
-        profileImage: "https://pbs.twimg.com/media/GCfHyvCaUAAODgW?format=jpg&name=4096x4096",
+      profileImage:
+        "https://pbs.twimg.com/media/GCfHyvCaUAAODgW?format=jpg&name=4096x4096",
       collectionArray: [
         "https://pbs.twimg.com/media/E-6_nIuVkAMu-63?format=jpg&name=4096x4096",
         "https://pbs.twimg.com/media/EaoHQGwU4AAVUGp?format=jpg&name=4096x4096",
@@ -48,7 +64,8 @@ const CategoryPage = () => {
     player: {
       bannerImage:
         "https://pbs.twimg.com/media/GNN87D1b0AAjMkv?format=jpg&name=4096x4096",
-    profileImage: "https://pbs.twimg.com/media/GNN87D1b0AAjMkv?format=jpg&name=4096x4096",
+      profileImage:
+        "https://pbs.twimg.com/media/GNN87D1b0AAjMkv?format=jpg&name=4096x4096",
       collectionArray: [
         "https://pbs.twimg.com/media/GMJBZ0_a4AAVb4T?format=jpg&name=4096x4096",
         "https://pbs.twimg.com/media/GLHyzbUaoAAqMuT?format=jpg&name=4096x4096",
@@ -57,14 +74,18 @@ const CategoryPage = () => {
     },
   };
 
-  console.log(id);
+  const filteredNFTData = dataArray.filter((el) => el.elemental.toLowerCase() === id);
   const selectedCollection = collections[id] || collections.forest;
+
+  // Use selectedCollection's images as default if filteredNFTData is empty
+  const bannerImage = filteredNFTData.length > 0 ? filteredNFTData[0].imageCover : selectedCollection.bannerImage;
+  const profileImage = filteredNFTData.length > 0 ? filteredNFTData[0].imageCover : selectedCollection.profileImage;
 
   return (
     <div className={Style.categoryPage}>
-      <Banner bannerImage={selectedCollection.bannerImage} />
-      <CollectionProfile profileImage={selectedCollection.profileImage}/>
-      <NFTCardTwo NFTData={selectedCollection.collectionArray} />
+      <Banner bannerImage={bannerImage} />
+      <CollectionProfile profileImage={profileImage} />
+      <NFTCardTwo NFTData={filteredNFTData} />
     </div>
   );
 };
