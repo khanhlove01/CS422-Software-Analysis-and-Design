bd6761daa45418bb97a23878f4db944f06c98278
Tran Minh Khanh <kidopro03@gmail.com>
2024-05-19 16:32:09 +0700

    Get single NFT

--- frontend/src/pages/nftDetails.jsx
+++ frontend/src/pages/nftDetails.jsx
@@ -3,11 +3,26 @@ import React from "react";
 //INTERNAL IMPORT
 import { Button, Category, Brand } from "../components/componentsindex";
 import NFTDetailsPage from "../nftDetailsPage/NFTDetailsPage";
-
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const NFTDetails = () => {
+  const { id } = useParams();
+  const [nft, setNft] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/v1/nfts/${id}`)
+      .then((res) => {
+        setNft(res.data.data.nft);
+        console.log(res.data.data.nft);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },[])
   return (
     <div>
-      <NFTDetailsPage/>
+      <NFTDetailsPage />
       <Category />
       <Brand />
     </div>
